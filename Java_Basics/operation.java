4b655ceecf2ec7b5ed1258978593bbd86d6ca2a1
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-18 23:23:13 +0530

    java operations

--- Java_Basics/operation.java
+++ Java_Basics/operation.java
@@ -1,3 +1,19 @@
 public class operation {
-    
+    public static void main(String[] args) {
+        double x = 5;
+        double y = 6;
+        double z = 20;
+        int a = 5;
+        int b = 7;
+        double c = Math.pow(a, b);
+
+        double sum = (x - y) + z;
+        double sum2 = (x * y) + z;
+        double sum3 = (x * y) / z;
+        System.out.println(sum);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(c);
+
+    }
 }
