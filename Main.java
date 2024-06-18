164e36dd38e41da0c503539f71cea6bf52baf54b
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-18 22:47:42 +0530

    Hello World

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello World! \n");
+        System.out.println("Hello Java! \n");
+        System.out.println("short is sout and enter!");
+    }
+}
+
+
