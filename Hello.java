002a7733b0ff20eda248760a86e2778d25c2951d
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-18 23:07:48 +0530

    type

--- Hello.java
+++ Hello.java
@@ -1,5 +1,17 @@
 public abstract class Hello {
     public static void main(String[] args){
 System.out.println("Hello");
-    } 
+int num=5;
+double num2=5.4;
+boolean b=false;
+char c='s';
+String s="suman";
+System.out.println(num);
+System.out.println(num2);
+System.out.println(b);
+System.out.println(c);
+System.out.println(s);
+} 
 }
+
+
