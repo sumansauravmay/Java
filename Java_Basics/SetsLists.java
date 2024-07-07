3c3fd7efa9a95e30482e2d263f3202eed28225ad
sumansauravmay <sumansauravmay12345@gmail.com>
2024-07-07 23:29:56 +0530

    set and list

---
+++ Java_Basics/SetsLists.java
@@ -0,0 +1,35 @@
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.HashSet;
+
+public class SetsLists {
+    public static void main(String[] args) {
+        // Set<Integer> t = new HashSet<Integer>();
+        // t.add(3);
+        // t.add(4);
+        // t.add(5);
+        // t.add(-8);
+        // t.add(3);
+        // t.remove(4);
+        // boolean x = t.contains(3);
+        // int y = t.size();
+        // boolean z = t.isEmpty();
+
+        // System.out.println(t);
+        // System.out.println(x);
+        // System.out.println(y);
+        // System.out.println(z);
+
+        Set<Integer> t = new TreeSet<Integer>();// sort the data in increasing
+        t.add(3);
+        t.add(4);
+        t.add(5);
+        t.add(-8);
+        t.add(3);
+        t.remove(4);
+
+        System.out.println(t);
+
+    }
+}
