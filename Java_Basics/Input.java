79ed6ea77b1190035032f31793b35bf20fff7ad6
sumansauravmay <sumansauravmay12345@gmail.com>
2024-07-07 18:37:50 +0530

    take input

--- Java_Basics/Input.java
+++ Java_Basics/Input.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
-        
+        // int x = 56 % 5;
+        Scanner sc = new Scanner(System.in);
+        String scanned = sc.next();// string- take this for all because anything ca be converted in the string.
+       int y=Integer.parseInt(scanned);
+        // int nums = sc.nextInt();// intiger
+        // double nums2 = sc.nextDouble();// float
+
+        // System.out.println(scanned);
+        System.out.println(y);
+        // System.out.println(x);
+        // System.out.println(nums);
+        // System.out.println(nums2);
     }
 }
