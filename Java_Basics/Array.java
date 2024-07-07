732b060ee3a007ecb7fb470fc49584cef98e8c30
sumansauravmay <sumansauravmay12345@gmail.com>
2024-07-07 19:44:51 +0530

    array

---
+++ Java_Basics/Array.java
@@ -0,0 +1,23 @@
+public class Array {
+    public static void main(String[] args) {
+        // int[] newArr = new int[5];
+        // newArr[0] = 1;
+        // newArr[1] = 2;
+        // newArr[2] = 3;
+        // newArr[3] = 4;
+        // newArr[4] = 5;
+
+
+        // String[] newArr = new String[5];
+        // newArr[0] = "Hello";
+        // newArr[1] = "World!";
+        // newArr[2] = "I";
+        // newArr[3] = "am";
+        // newArr[4] = "Suman Saurav";
+
+int[] newArr={1,2,4,6,8,4};
+
+
+        System.out.println(newArr[4]);
+    }
+}
